f4cb66ff8607ebdd0bb275549ab94048baa9ef0b
CallMeHein <heinrichmakovenko@gmail.com>
2024-11-09 02:11:36 +0100

    add lilac to non-terrain blocks

--- src/main/java/hein/auto_western_highway/Blocks.java
+++ src/main/java/hein/auto_western_highway/Blocks.java
@@ -45,6 +45,7 @@ public class Blocks {
                 "fern",
                 "flower",
                 "leaves",
+                "lilac",
                 "lily_pad",
                 "nether",
                 "obsidian",
