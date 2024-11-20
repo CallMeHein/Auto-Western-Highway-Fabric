4569bc019b79ab75fa77df8ae72768b9ddec06f1
CallMeHein <heinrichmakovenko@gmail.com>
2024-11-20 15:37:41 +0100

    make logout communicate with the server to immediately disconnect instead of just killing the client

--- src/main/java/hein/auto_western_highway/common/NightLogout.java
+++ src/main/java/hein/auto_western_highway/common/NightLogout.java
@@ -2,8 +2,9 @@ package hein.auto_western_highway.common;
 
 import baritone.api.BaritoneAPI;
 import baritone.api.process.IBuilderProcess;
-import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.packet.s2c.common.DisconnectS2CPacket;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 
@@ -13,7 +14,7 @@ import static hein.auto_western_highway.common.Utils.*;
 
 public class NightLogout {
     // not the actual values; rounded and padded to allow for some wiggle room without letting mobs spawn
-    private static final int FIRST_MOB_SPAWN_TICK = 12850; // ~7:30PM
+    private static final int FIRST_MOB_SPAWN_TICK = 12800; // ~7:30PM
     private static final int LAST_MOB_SPAWN_TICK = 23050; // ~5AM
 
     public static void nightLogout() {
@@ -30,8 +31,10 @@ public class NightLogout {
 
             BlockPos logoutPos = getPlayerFeetBlock(globalPlayerNonNull.get());
 
-            DisconnectedScreen screen = new DisconnectedScreen(null, Text.literal("AWH: Disconnected to avoid the night"), Text.literal("Waiting for Meteor to Auto-Reconnect"));
-            globalClient.get().execute(() -> globalClient.get().disconnect(screen));
+            ClientConnection connection = globalClient.get().getNetworkHandler().getConnection();
+            connection.send(new DisconnectS2CPacket(Text.literal("AWH: Disconnected to avoid the night - waiting for Meteor to reconnect")));
+            connection.disconnect(Text.literal("AWH: Disconnected to avoid the night - waiting for Meteor to reconnect"));
+
             sleep(3000);
             waitUntilTrue(() -> {
                 ClientPlayerEntity _player = globalPlayer.get();
